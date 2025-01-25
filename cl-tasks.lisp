585058190b2686feadffa6a720e9796be5ea6f2d
fieldvoid <58522003+fieldvoid@users.noreply.github.com>
2025-01-25 14:48:39 -0500

    cl-tasks.lisp - Add `task-finised` condition and a call method

--- cl-tasks.lisp
+++ cl-tasks.lisp
@@ -14,3 +14,18 @@
        ,@(loop for expr in exprs
                collect `(vector-push-extend (lambda () ,expr) ,array))
        (make-instance 'task :expr-array ,array))))
+
+(define-condition task-finished ()
+  ((task :initarg :task :accessor task))
+  (:report (lambda (self stream)
+             (declare (ignore self))
+             (format stream "Task is complete. No more instructions available."))))
+
+(defmethod call ((obj task))
+  (with-slots (expr-array index) obj
+    (let ((len (length expr-array)))
+      (if (< index len)
+          (let ((i index))
+            (incf index)
+            (funcall (aref expr-array i)))
+          (signal 'task-finished :task obj)))))
