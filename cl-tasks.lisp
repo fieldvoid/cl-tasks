ba325ef80eea6d397d8b27dd78206b05093ec846
fieldvoid <58522003+fieldvoid@users.noreply.github.com>
2025-01-25 14:37:14 -0500

    Create cl-tasks.lisp

---
+++ cl-tasks.lisp
@@ -0,0 +1,16 @@
+(defclass task ()
+  ((expr-array :initargs :expr-array)
+   (index)
+   (mailbox)))
+
+(defmethod initialize-instance :after ((obj task) &rest rest)
+  (declare (ignore rest))
+  (with-slots (index) obj
+    (setf index 0)))
+
+(defmacro make-task (&rest exprs)
+  (let ((array (gensym)))
+    `(let ((,array (make-array 0 :adjustable t :fill-pointer 0)))
+       ,@(loop for expr in exprs
+               collect `(vector-push-extend (lambda () ,expr) ,array))
+       (make-instance 'task :expr-array ,array))))
